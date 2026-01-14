1dac22c0e225a19c622fa401e03d42c10539a45b
Rajatg180 <rajatgore1803@gmail.com>
2026-01-14 20:34:36 +0530

    changed deployed url

--- issueflow_fronted/lib/core/config/app_config.dart
+++ issueflow_fronted/lib/core/config/app_config.dart
@@ -1,17 +1,30 @@
-import 'package:flutter/foundation.dart';
+// import 'package:flutter/foundation.dart';
+
+// class AppConfig {
+//   // Change this to your PC LAN IP (for real devices)
+//   static const String _lanIpBaseUrl = "http://192.168.1.5:8000";
+
+//   // Localhost for web/desktop when backend runs on same machine
+//   static const String _localBaseUrl = "http://127.0.0.1:8000";
+
+//   static String get baseUrl {
+//     // Flutter Web runs on your PC → use localhost
+//     if (kIsWeb) return _localBaseUrl;
+
+//     // Mobile real device → use LAN IP
+//     return _lanIpBaseUrl;
+//   }
+// }
+
+
 
 class AppConfig {
-  // Change this to your PC LAN IP (for real devices)
-  static const String _lanIpBaseUrl = "http://192.168.1.5:8000";
-
-  // Localhost for web/desktop when backend runs on same machine
-  static const String _localBaseUrl = "http://127.0.0.1:8000";
+  // Render backend URL
+  static const String _productionBaseUrl =
+      "https://issueflow-project-backend.onrender.com";
 
   static String get baseUrl {
-    // Flutter Web runs on your PC → use localhost
-    if (kIsWeb) return _localBaseUrl;
-
-    // Mobile real device → use LAN IP
-    return _lanIpBaseUrl;
+    // Use production backend for everything now
+    return _productionBaseUrl;
   }
 }
